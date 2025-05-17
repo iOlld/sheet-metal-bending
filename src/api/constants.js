371aabf05d151d0ce381d07d8cfd108f99195af0
iOlld <evgeniy.iolld@gmail.com>
2025-05-17 14:00:22 +0300

    added new files and component Layout

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,11 +1,11 @@
 import { baseConstants } from '@/api/_utils/constants.js'
 
-export const products = {
-  ...baseConstants,
-  stripe: { value: 'STRIPE', label: 'Полоса', price: 3 },
-  plank: { value: 'PLANK', label: 'Планка', price: 3.57 },
-  gardenBed: { value: 'GARDEN_BED', label: 'Грядка', price: 7.05 },
-}
+// export const products = {
+//   ...baseConstants,
+//   stripe: { value: 'STRIPE', label: 'Полоса', price: 3 },
+//   plank: { value: 'PLANK', label: 'Планка', price: 3.57 },
+//   gardenBed: { value: 'GARDEN_BED', label: 'Грядка', price: 7.05 },
+// }
 
 export const bendingTables = {
   ...baseConstants,
---
+++ src/components/ui/layout/Layout.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col size-full">
+    <slot name="header"/>
+    <div class="flex flex-col h-full flex-1 gap-2">
+      <slot />
+    </div>
+  </div>
+</template>
